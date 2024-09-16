e67fe7d29b732a300868b7377ca0fbb382682016
todd-estes <elestes@gmail.com>
2024-09-16 17:14:41 -0400

    feat: Add SearchBooks component
    - Compares currently shelved books against search
    results. If a result is currently on shelf, that
    result will be replaced with the stateful shelved
    book object

--- starter/src/SearchBooks.js
+++ starter/src/SearchBooks.js
@@ -1,24 +1,82 @@
 import React from 'react'
+import { useState, useEffect, useCallback } from 'react';
+import { Link } from "react-router-dom";
+import * as BooksAPI from "./BooksAPI.js";
+import Shelf from "./Shelf";
 
-function SearchBooks({ showSearchPage, setShowSearchpage}) {
+const SearchBooks = ({ shelvedBooks, onUpdateBook}) => {
+  const [query, setQuery] = useState('')
+  const [queriedBooks, setQueriedBooks] = useState([])
+
+  const updateQuery = (v) => {
+    setQuery(v);
+  }
+
+  const orderBookByTitle = (a, b) =>  {
+    return a.title.localeCompare(b.title);
+  }
+
+  const updateQueriedBooks = useCallback((results) => {
+    const resultIDs = results.map((result) => result.id);
+    const shelvedBooksInQuery = shelvedBooks.filter((book) =>
+      resultIDs.includes(book.id)
+    );
+
+    const shelvedIDs = shelvedBooksInQuery.map((book) => book.id);
+    const newBooksInQuery = results.filter(
+      (result) => !shelvedIDs.includes(result.id)
+    );
+
+    const allBooks = [...shelvedBooksInQuery, ...newBooksInQuery];
+    const sortedBooks = [...allBooks].sort(orderBookByTitle);
+
+    setQueriedBooks(sortedBooks);
+  }, [shelvedBooks]);
+
+
+   useEffect(() => {
+    const searchBooks = async () => {
+      if (query) { // Only search if query is not empty
+        try {
+          const res = await BooksAPI.search(query.trim());
+          if (res && !res.error) {
+            updateQueriedBooks(res);
+          } else {
+            setQueriedBooks([])
+          }
+        } catch (error) {
+          console.error("Error searching books:", error);
+          setQueriedBooks([]);
+          }
+      } else {
+        setQueriedBooks([]);
+      }
+    }
+
+    searchBooks();
+   }, [query, updateQueriedBooks]);
+
   return (
     <div className="search-books">
       <div className="search-books-bar">
-        <a
-          className="close-search"
-          onClick={() => setShowSearchpage(!showSearchPage)}
-        >
+        <Link to="/" className="close-search">
           Close
-        </a>
+        </Link>
         <div className="search-books-input-wrapper">
           <input
             type="text"
             placeholder="Search by title, author, or ISBN"
+            value={query}
+            onChange={(event) => updateQuery(event.target.value)}
           />
         </div>
       </div>
       <div className="search-books-results">
-        <ol className="books-grid"></ol>
+        <ol className="books-grid">
+        {queriedBooks.length > 0 && (
+          <Shelf books={queriedBooks} onUpdateBook={onUpdateBook}/>
+        )}
+        </ol>
       </div>
     </div>
   )
